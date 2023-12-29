8b392a57d9251c0ffcdc7dbaced79831aedb375a
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-12-30 06:40:10 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,11 +1,36 @@
 var videoElement;
+// Define a variable to track whether there are ads loaded and initially set it to false
+var adsLoaded = false;
 
-// On window load, attach an event to the play button click
-// that triggers playback on the video element
 window.addEventListener('load', function(event) {
   videoElement = document.getElementById('video-element');
+  initializeIMA();
+  videoElement.addEventListener('play', function(event) {
+    loadAds(event);
+  });
   var playButton = document.getElementById('play-button');
   playButton.addEventListener('click', function(event) {
     videoElement.play();
   });
 });
+
+window.addEventListener('resize', function(event) {
+  console.log("window resized");
+});
+
+function initializeIMA() {
+  console.log("initializing IMA");
+}
+
+function loadAds(event) {
+  // Prevent this function from running on if there are already ads loaded
+  if(adsLoaded) {
+    return;
+  }
+  adsLoaded = true;
+
+  // Prevent triggering immediate playback when ads are loading
+  event.preventDefault();
+
+  console.log("loading ads");
+}
