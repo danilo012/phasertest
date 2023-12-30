9135fc8f28f4a0b6dfb3367e6289c26af07f816e
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-12-30 08:02:50 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -50,7 +50,7 @@ function setUpIMA() {
 
   // Request video ads.
   const adsRequest = new google.ima.AdsRequest();
-  adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
+  adsRequest.adTagUrl = 'https://glugherg.net/4/6659427' +
       'iu=/21775744923/external/single_ad_samples&sz=640x480&' +
       'cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&' +
       'output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=';
